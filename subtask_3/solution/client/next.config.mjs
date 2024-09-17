8affe3254b4828781e91172453ea519d5c4f41c8
Jitendra2603 <140426495+Jitendra2603@users.noreply.github.com>
2024-09-18 00:58:52 +0530

    Update next.config.mjs

--- subtask_3/solution/client/next.config.mjs
+++ subtask_3/solution/client/next.config.mjs
@@ -1,3 +1,4 @@
+// next.config.mjs
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -5,6 +6,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const nextConfig = {
+  experimental: {
+    appDir: true, // Add this line to enable the experimental app directory
+  },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
@@ -13,6 +17,5 @@ const nextConfig = {
     return config;
   }
 };
-
 
 export default nextConfig;
