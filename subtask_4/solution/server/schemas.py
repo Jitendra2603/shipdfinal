71690d6f84f35df3e76374c8886a742f5f84ba65
Jitendra2603 <140426495+Jitendra2603@users.noreply.github.com>
2024-09-21 10:17:26 +0530

    Update schemas.py

--- subtask_4/solution/server/schemas.py
+++ subtask_4/solution/server/schemas.py
@@ -1,5 +1,9 @@
 from pydantic import BaseModel
 
-class CodeResult(BaseModel):
+class CodeResultSchema(BaseModel):
+    id: int
     code: str
     output: str
+
+    class Config:
+        orm_mode = True
