11d7dbe7a1cb7e08a34b1d4df3ee19c770e3fce5
Jitendra2603 <140426495+Jitendra2603@users.noreply.github.com>
2024-09-21 10:14:26 +0530

    Update database.py

--- subtask_4/solution/server/database.py
+++ subtask_4/solution/server/database.py
@@ -1,9 +1,14 @@
+# server/database.py
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL","postgresql://user:password@postgresserver/db")
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:password@localhost:5432/postgres"
+)
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
